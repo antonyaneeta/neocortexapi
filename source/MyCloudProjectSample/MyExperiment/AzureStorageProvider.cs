e64888c5da183fa916e27d5144a580aba44d43fc
Aneeta Antony <aneetakalathoor@gmail.com>
2023-07-07 13:53:44 +0200

    Changes to upload file into azure storage blob container.

--- source/MyCloudProjectSample/MyExperiment/AzureStorageProvider.cs
+++ source/MyCloudProjectSample/MyExperiment/AzureStorageProvider.cs
@@ -7,6 +7,7 @@ using MyCloudProject.Common;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -33,27 +34,27 @@ namespace MyExperiment
                 // Get a reference to a blob named "sample-file"
                 BlobClient blob = container.GetBlobClient(fileName);
 
-            //throw if not exists:
-            //blob.ExistsAsync
+                //throw if not exists:
+                //blob.ExistsAsync
 
-            // return "../myinputfilexy.csv"
-            // Download the blob's contents and save it to a file
-            BlobDownloadInfo download = await blob.DownloadAsync();
+                // return "../myinputfilexy.csv"
+                // Download the blob's contents and save it to a file
+                BlobDownloadInfo download = await blob.DownloadAsync();
 
-            using (FileStream file = File.OpenWrite(fileName))
-            {
-                 download.Content.CopyTo(file);
+                using (FileStream file = File.OpenWrite(fileName))
+                {
+                    download.Content.CopyTo(file);
 
-                    return file.Name ;
+                    return file.Name;
                 }
 
-            
+
             }
             catch (Exception ex)
             {
                 throw new NotImplementedException(); ;
             }
-           
+
             // throw new NotImplementedException();
         }
 
@@ -70,19 +71,36 @@ namespace MyExperiment
                 Accuracy = (float)0.5,
             };
 
-         
+
             await client.UpsertEntityAsync((ExperimentResult)result);
 
         }
 
         public async Task<byte[]> UploadResultFile(string fileName, byte[] data)
         {
+            BlobContainerClient container = new BlobContainerClient(this.config.StorageConnectionString, "outblobcontainer-1");
+            await container.CreateIfNotExistsAsync();
+
+            try
+            {
+                // Get a reference to a blob 
+                BlobClient blob = container.GetBlobClient(fileName);
+
+                // Upload file data
+                await blob.UploadAsync(fileName);
 
 
-            throw new NotImplementedException();
+                // Verify we uploaded some content
+                BlobProperties properties = await blob.GetPropertiesAsync();
+                Console.WriteLine(properties.ToString());
+                return data;
+            }
+            finally
+            {
+                // await container.DeleteIfExistsAsync();
+            }
+        }
         }
 
-    }
-
-
+    
 }
